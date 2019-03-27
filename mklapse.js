6d5688fe8177063948fe638cd34bebe1daa62586
zvakanaka <quintonish@gmail.com>
2019-03-26 20:55:18 -0600

    move args parser out to lib and show percentages

--- mklapse.js
+++ mklapse.js
@@ -1,63 +1,40 @@
+const {spawn} = require('child_process');
 const exec = require('util').promisify(require('child_process').exec);
 const sequentialPromiseAll = require('./lib/sequentialPromiseAll');
+const barChart = require('bar-charts');
 const fileParts = require('./lib/fileParts');
 const fs = require('fs');
 const {promisify} = require('util');
 const readdir = promisify(fs.readdir);
-
+const understand = require('./lib/understand');
 const DEFUALT_TEMP_DIR = 'mklapse';
 const pwd = './';
 
 module.exports = mklapse;
 
 async function mklapse(inputArgs) {
-  const operationType = inputArgs[0];
   const files = await readdir(pwd);
   const validFiles = files.filter(fileName => {
     return fileName.toLowerCase().endsWith('.jpg');
   });
-  const options = {};
   const optionsConfig = [
-    {operationType: 'trails'},
-    {operationType: 'zoom', name: 'delta', alias: 'd', defaultValue: 1.001, type: Number},
-    {operationType: 'play'},
-    {operationType: 'video', name: 'framerate', alias: 'r', defaultValue: 30, type: Number}
+    {command: 'trails'},
+    {command: 'zoom', name: 'delta', alias: 'd', defaultValue: 1.001, type: Number},
+    {command: 'resize', name: 'percentage', alias: 'p', defaultValue: '25%', type: String},
+    {command: 'play'},
+    {command: 'video', name: 'framerate', alias: 'r', defaultValue: 30, type: Number}
   ];
   // set up defaults
-  optionsConfig
-    .filter(c => c.operationType === operationType)
-    .forEach((c) => {
-      options[c.name] = c.defaultValue;
-    });
-  // overlay command line arguments onto the config
-  if (inputArgs.length > 1) {
-    const matchingOptionsConfigs = optionsConfig.filter(c => c.operationType === operationType);
-    const args = inputArgs.slice(1); // remove operationType
-    let giveMeABreak = false;
-    args.forEach((cur, i) => {
-      // --delta value => name: delta
-      if (!giveMeABreak) {
-        const refType = cur.startsWith('--') ? 'name' : cur.startsWith('-') ? 'alias' : false; // name or alias
-        if (!refType) throw new Error(`Unknown command line argument, '${cur}'`);
-        const refName = cur.startsWith('--') ? cur.substr(2) : cur.startsWith('-') ? cur.substr(1) : cur;
-        if (refType) {
-          const matchingConfig = matchingOptionsConfigs.find(c => c[refType] === refName);
-          if (!matchingConfig) throw new Error(`Unknown command line argument, '${cur}'`);
-          const type =  matchingConfig.type ? matchingConfig.type : (val) => val;
-          options[refName] = type(args[i + 1]);
-          giveMeABreak = true;
-        }
-      } else giveMeABreak = false;
-    });
-  }
+  const options = understand(optionsConfig);
+
   console.log('options', options);
-  if (operationType === 'video') mkvideo(validFiles, options);
-  else if (operationType === 'play') play();
-  else if (optionsConfig.map(c => c.operationType).includes(operationType)) mkphotos({validFiles, operationType, options});
+  if (options.command === 'video') mkvideo(validFiles, options);
+  else if (options.command === 'play') play();
+  else if (optionsConfig.map(c => c.command).includes(options.command)) mkphotos({validFiles, options});
   else console.log('invalid parameter(s) specified');
 };
 
-async function mkphotos({validFiles, operationType, options}) {
+async function mkphotos({validFiles, options}) {
   await clean();
   await mkdir(DEFUALT_TEMP_DIR);
   await exec(`cp ${validFiles[0]} ${DEFUALT_TEMP_DIR}/IMG_0000.jpg`);
@@ -71,7 +48,7 @@ async function mkphotos({validFiles, operationType, options}) {
     count = `${Number(count) + 1}`.padStart(4, '0');
     const outputFile = `${DEFUALT_TEMP_DIR}/IMG_${count}.jpg`;
     let command;
-    switch (operationType) {
+    switch (options.command) {
       case 'zoom':
         command = `convert ${inputFile} -resize ${width}x${height}^ -gravity center -extent ${originalWidth}x${originalHeight} ${outputFile}`;
         width *= options.delta;
@@ -80,8 +57,11 @@ async function mkphotos({validFiles, operationType, options}) {
       case 'trails':
         command = `convert ${DEFUALT_TEMP_DIR}/IMG_${last}.jpg ${inputFile} -gravity center -compose lighten -composite -format jpg ${outputFile}`;
         break;
+      case 'resize':
+        command = `convert -resize ${options.percentage} ${inputFile} ${outputFile}`;
+        break;
       default:
-        throw new Error(`Invalid operation type specified, '${operationType}'`);
+        throw new Error(`Invalid operation type specified, '${options.command}'`);
     }
     return command;
   });
@@ -92,23 +72,72 @@ async function mkphotos({validFiles, operationType, options}) {
     if (args[0]) args[0] = commandsArray[++commandIndex];
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
-    process.stdout.write(`${commandIndex + 1}/${commandsArray.length}`); // end the line
+    const count = parseInt(commandIndex, 10) / commandsArray.length * 100;
+    const blank = '⠀'; // braille blank emoji (because jstrace-bars removes leading spaces)
+    const output = barChart([{label: `${`${parseInt(commandIndex, 10) + 1}`.padStart(`${commandsArray.length}`.length, blank)}/${commandsArray.length}`, count}], {percentages: true});
+    process.stdout.write(output);
   });
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
+  process.stdout.write(); // end the line
   console.log();
+  console.log(`${commandIndex + 1}/${commandsArray.length}`);
 }
 
 async function mkvideo(validFiles, options) {
   const {ext: extension, fileWithoutExt: filePrefix} = fileParts(validFiles[0]);
   const [, startNumber] = filePrefix.match(/(\d{4})/);
-  const ffmpegCommand = `ffmpeg -y -framerate ${options.framerate} -start_number "${startNumber}" -i IMG_%04d.${extension} -s:v 1080x720 -c:v libx264 -pix_fmt yuv420p -r ${options.framerate} $(basename $(pwd)).mp4`;
-  await exec(ffmpegCommand);
+  const ffmpegCommand = `ffmpeg -y -framerate ${options.framerate} -start_number "${startNumber}" -i IMG_%04d.${extension} -s:v 1080x720 -c:v libx264 -pix_fmt yuv420p -r ${options.framerate} ${DEFUALT_TEMP_DIR}.mp4`;
+  const args = ffmpegCommand.split(' ').slice(1);
+  console.log('RUNNING', args);
+  try {
+    const ffmpeg = spawn('ffmpeg', args, {shell: true});
+    ffmpeg.stdout.on('data', function(data) { console.log('data:', data); });
+    ffmpeg.stderr.on('data', function(data) {
+      const text = data.toString('utf8');
+      const regexArr = text.match(/frame=\s+(\d+)\sfps=\s?(\d+\.?\d*)\sq=(\d+\.?\d*)\ssize=\s+(\d+)kB\stime=(\d{2}:\d{2}:\d{2}\.\d{2})\sbitrate=\s*((N\/A)|(\d+\.?\d*))(kbits\/s)?\sspeed=\s*(\d\.?\d*e?-?\d*)x?/);
+      if (regexArr) {
+        const [, frame, fps, q, size, time] = regexArr;
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        const count = parseInt(frame, 10) / validFiles.length * 100;
+        const blank = '⠀'; // braille blank emoji (because jstrace-bars removes leading spaces)
+        const output = barChart([{label: `${`${parseInt(frame, 10) + 1}`.padStart(`${validFiles.length}`.length, blank)}/${validFiles.length}`, count}], {percentages: true});
+        process.stdout.write(output); // end the line
+      }
+    });
+
+
+    ffmpeg.stdout.on('end', function(data) {
+     data && console.log('end', data);
+    });
+    ffmpeg.on('exit', function(code) {
+      if (code != 0) console.log('Error code: ' + code);
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+      console.log(`${validFiles.length}/${validFiles.length}`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-async function play() {
-  const ffplayCommand = `ffplay -loop 0 $(basename $(pwd)).mp4`;
-  await exec(ffplayCommand);
+function play() {
+  const ffplayCommand = `ffplay -loop 0 ${DEFUALT_TEMP_DIR}.mp4`;
+  const args = ffplayCommand.split(' ').slice(1);
+  const ffplay = spawn('ffplay', args);
+
+  console.log('RUNNING', args);
+
+  ffplay.stdout.on('data', function(data) { console.log('data:', data); });
+  ffplay.stdout.on('end', function(data) {
+   console.log('end', data);
+  });
+  ffplay.on('exit', function(code) {
+    if (code != 0) {
+      console.log('Failed: ' + code);
+    }
+  });
 }
 
 async function mkdir(dirname) {
